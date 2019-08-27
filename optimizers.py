c796d5c4875d5fb07926c402dd1ede49269cd31c
Michele <31963799+micheleciciolla@users.noreply.github.com>
2019-08-27 17:26:26 +0200

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -1,5 +1,3 @@
-# Ottima implementazione di uno shift based per AdaMax Optimizer, trovata in rete
-
 import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
@@ -7,53 +5,56 @@ from tensorflow.python.ops import state_ops
 from tensorflow.python.framework import ops
 from tensorflow.python.training import optimizer
 
-import layers
-ap2 = layers.ap2
+import michele_binNN.layers as layers
 
-# keras.optimizers.Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+ap2 = layers.ap2
 
 class ShiftBasedAdaMaxOptimizer(optimizer.Optimizer):
 	
-	def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8,
-					use_locking=False, name="ShiftBasedAdaMax"):
-		super(ShiftBasedAdaMaxOptimizer, self).__init__(use_locking, name)    # SUPERCLASSE
+	def __init__(self,
+				 learning_rate=0.001,
+				 beta1=0.9,
+				 beta2=0.999,
+				 epsilon=1e-8,
+				use_locking=False,
+				 name="ShiftBasedAdaMax"):
+
+		super(ShiftBasedAdaMaxOptimizer, self).__init__(use_locking, name)
 		
-		# primi valori
-		self.lr = learning_rate		 
-		self.beta1 = beta1
-		self.beta2 = beta2
-		self.epsilon = epsilon
-		
-		# val successivi non ancora assegnati
+		self.lr = learning_rate
+		self.beta1 = beta1 # betas of adam opt
+		self.beta2 = beta2 #  betas of adam opt
+		self.epsilon = epsilon  # Is a very small number to prevent any division by zero in the implementation
+
+		# updated params of SB_adamax
 		self._lr_t = None
 		self._beta1_t = None
 		self._beta2_t = None
 		self._eps_t = None
 
-
-	# metodo che connette i valori successivi a quelli precedenti creando tensori e quindi una rete connessa
+	# Create all needed tensors before applying gradients.
 	def _prepare(self):
+		# used to create tensors
 		self._lr_t = ops.convert_to_tensor(self.lr, name="learning_rate")
 		self._beta1_t = ops.convert_to_tensor(self.beta1, name="beta1")
 		self._beta2_t = ops.convert_to_tensor(self.beta2, name="beta2")
 		self._eps_t = ops.convert_to_tensor(self.epsilon, name="epsilon")
 	
-
-	# passare esplicitamente la lista delle variabili
+	
 	def _create_slots(self, var_list):
 		first_var = min(var_list, key=lambda x: x.name)
 		self._create_non_slot_variable(initial_value=self.beta1, name="beta1_power",  colocate_with=first_var)
 		
 		for v in var_list:
-			self._zeros_slot(v, "m", self._name)  ####### credo sia errore : ci va m
+			self._zeros_slot(v, "m", self._name)
 			self._zeros_slot(v, "v", self._name)
-
-	# ottieni il tensor-graph della variabile beta contenuta nello slot
+	
+	
 	def _get_beta_accumulator(self):
 		graph = ops.get_default_graph()
 		return self._get_non_slot_variable("beta1_power", graph=graph)
 	
-	# Main method
+	
 	def _apply_dense(self, grad, var):
 		
 		m = self.get_slot(var, "m")
@@ -61,34 +62,48 @@ class ShiftBasedAdaMaxOptimizer(optimizer.Optimizer):
 		
 		_beta1_power = self._get_beta_accumulator();
 		
-		# assegnazione dtype
-		beta1_power = math_ops.cast(_beta1_power, grad.dtype.base_dtype) #cast accomuna e fonde due type
+		beta1_power = math_ops.cast(_beta1_power, grad.dtype.base_dtype)
 		lr = math_ops.cast(self._lr_t, grad.dtype.base_dtype)
 		beta1 = math_ops.cast(self._beta1_t, grad.dtype.base_dtype)
 		beta2 = math_ops.cast(self._beta2_t, grad.dtype.base_dtype)
 		eps = math_ops.cast(self._eps_t, grad.dtype.base_dtype)
-		
-		# USO FORMULE PAPER
-		# NON DOVREBBE ESSERE IL CONTRARIO (m scambiato con v) ? ? ? ? 
-		v_t = v.assign(beta1 * v + (1. - beta1) * grad)
+
+		# FIRST TWO OPERATIONS on momentum
 		m_t = m.assign(tf.maximum(beta2 * m + eps, tf.abs(grad)))
-		
+		v_t = v.assign(beta1 * v + (1. - beta1) * grad)
+
 		# here we apply the shift on the learning rate and on m moment. Since the shift operation
 		# is defined for integer values only, we compute them as the approximate power of two
 		# of the original operations (lr/(1-beta1^t)) and (v_t / m_t). With dedicated hardware
 		# these operations would have been done just by using shift ops.
 
-		lr_c = ap2(lr / (1 - beta1_power))  #Primo termine formula
-		g_t = v_t / ap2(m_t)     #Secondo termine formula
-		var_update = state_ops.assign_sub(var, lr_c * g_t)  #assegna la moltiplicazione a var (aggiornandola)
+		lr_c = ap2(lr / (1 - beta1_power))
+		g_t = v_t / ap2(m_t)
 
-		return control_flow_ops.group(*[var_update, m_t, v_t])   #return aggiornamento completo gruppo variabili
-
+		# UPDATES OF VARIABLES
+		var_update = state_ops.assign_sub(var, lr_c * g_t)
+		return control_flow_ops.group(*[var_update, m_t, v_t])
+		
+		
 	def _apply_sparse(self, grad, var):
+		# override base method, so it's out particular case
 		raise NotImplementedError("Sparse gradient updates are not supported.")
 
-	# check finale
+
 	def _finish(self, update_ops, name_scope):
+		'''
+		This is called with the `name_scope` using the "name" that
+    	users have chosen for the application of gradients.
+
+		Args:
+     	update_ops: List of `Operation` objects to update variables.  This list
+        contains the values returned by the `_apply_dense()` and
+        `_apply_sparse()` calls.
+     	name_scope: String.  Name to use for the returned operation.
+
+    	Returns:
+      	The operation to apply updates.
+		'''
 		# Update the power accumulator.
 		with ops.control_dependencies(update_ops):
 			beta1_power = self._get_beta_accumulator()
